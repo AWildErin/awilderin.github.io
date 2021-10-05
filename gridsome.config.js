0ec31544a825e48f088599ea24b0572703fdb56a
Erin <28122025+AWildErin@users.noreply.github.com>
2021-10-05 22:24:57 +0100

    Initial commit

---
+++ gridsome.config.js
@@ -0,0 +1,26 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteUrl: 'https://awilderin.github.io',
+  titleTemplate: '%s | AWildErin',
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        typeName: 'Projects',
+        path: './content/projects/*.toml',
+      }
+    },
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        typeName: 'GeneralInfo',
+        path: './content/general.toml',
+      }
+    }
+  ]
+}
